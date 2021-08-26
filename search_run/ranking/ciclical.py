4416e8a428cede0067c98a2b8d1d53519428714c
JeanCarloMachado <jean.machado@getyourguide.com>
2021-08-26 21:39:56 +0200

    move cyclical ranking to its own module

---
+++ search_run/ranking/ciclical.py
@@ -0,0 +1,67 @@
+from search_run.logger import configure_logger
+
+logger = configure_logger()
+
+
+class CiclicalPlacement:
+    def cyclical_placment(self, entries, commands_performed):
+        """Put 1 result of natural rank after 1 result of visits"""
+
+        used_items = self.compute_used_items_score(entries, commands_performed)
+        natural_position = self.compute_natural_position_scores(entries)
+
+        result = []
+        used_keys = []
+        total_items = len(entries)
+        position = 0
+        while len(natural_position) > 0:
+
+            if position % 2 == 0 and len(used_items) > 0:
+                key = used_items.pop(0)
+            else:
+                key = natural_position.pop(0)
+
+            if key in used_keys:
+                continue
+
+            result.append((key, entries[key]))
+            used_keys.append(key)
+            position = position + 1
+
+        return result
+
+    def compute_used_items_score(self, entries, commands_performed):
+        # a list with the keys of used entries, separated by space
+        used_items = commands_performed["key"].tolist()
+
+        # linear decay for use
+        total_used_items = len(used_items)
+        scores_used_items = {}
+        for position, key in enumerate(used_items):
+            if key not in entries:
+                logger.info(f"key not in entries: {key}")
+                continue
+
+            score = (total_used_items - position) / total_used_items
+
+            if key in scores_used_items:
+                aggregation = score + (scores_used_items[key] * (1 / position))
+                score = aggregation if aggregation < 1 else 1
+
+            scores_used_items[key] = score
+        used_items = sorted(scores_used_items, key=scores_used_items.get, reverse=True)
+
+        return used_items
+
+    def compute_natural_position_scores(self, entries):
+        # quadratic decay for position
+        total_items = len(entries)
+        natural_position_scored = {}
+        for position, (key, value) in enumerate(entries.items()):
+            natural_position_scored[key] = total_items / (total_items + position)
+
+        natural_position = sorted(
+            natural_position_scored, key=natural_position_scored.get, reverse=True
+        )
+
+        return natural_position
--- search_run/ranking/ranking.py
+++ search_run/ranking/ranking.py
@@ -5,8 +5,7 @@ import pandas as pd
 import json
 
 from search_run.logger import configure_logger
-
-logger = configure_logger()
+from search_run.ranking.ciclical import CiclicalPlacement
 
 
 class Ranking:
@@ -27,73 +26,9 @@ class Ranking:
         entries: dict = self.configuration().commands
         commands_performed = self.load_commands_performed_df()
 
-        result = self.cyclical_placment(entries, commands_performed)
+        result = CiclicalPlacement().cyclical_placment(entries, commands_performed)
 
         return self._export_to_file(result)
-
-    def cyclical_placment(self, entries, commands_performed):
-        """Put 1 result of natural rank after 1 result of visits"""
-
-        used_items = self.compute_used_items_score(entries, commands_performed)
-        natural_position = self.compute_natural_position_scores(entries)
-
-        result = []
-        used_keys = []
-        total_items = len(entries)
-        position = 0
-        while len(natural_position) > 0:
-
-            if position % 2 == 0 and len(used_items) > 0:
-                key = used_items.pop(0)
-            else:
-                key = natural_position.pop(0)
-
-            if key in used_keys:
-                continue
-
-            result.append((key, entries[key]))
-            used_keys.append(key)
-            position = position + 1
-
-        return result
-
-    def compute_used_items_score(self, entries, commands_performed):
-
-        # a list with the keys of used entries, separated by space
-        used_items = commands_performed["key"].tolist()
-
-        # linear decay for use
-        total_used_items = len(used_items)
-        scores_used_items = {}
-        for position, key in enumerate(used_items):
-            if key not in entries:
-                logger.info(f"key not in entries: {key}")
-                continue
-
-            score = (total_used_items - position) / total_used_items
-
-            if key in scores_used_items:
-                aggregation = score + (scores_used_items[key] * (1 / position))
-                score = aggregation if aggregation < 1 else 1
-
-            scores_used_items[key] = score
-        used_items = sorted(scores_used_items, key=scores_used_items.get, reverse=True)
-
-        return used_items
-
-    def compute_natural_position_scores(self, entries):
-        # quadratic decay for position
-        total_items = len(entries)
-        natural_position_scored = {}
-        for position, (key, value) in enumerate(entries.items()):
-
-            natural_position_scored[key] = total_items / (total_items + position)
-
-        natural_position = sorted(
-            natural_position_scored, key=natural_position_scored.get, reverse=True
-        )
-
-        return natural_position
 
     def load_commands_performed_df(self):
         with open("/data/grimoire/message_topics/run_key_command_performed") as f:
