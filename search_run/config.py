c9712c1b5d6ffca2922a890dd2533ea4bea8cc91
JeanCarloMachado <jean.machado@getyourguide.com>
2021-09-03 17:00:28 +0200

    automatic commit

--- search_run/config.py
+++ search_run/config.py
@@ -1,3 +1,3 @@
 PROJECT_ROOT = "/home/jean/projects/grimoire/grimoire/search_run"
-MAIN_FILE = f"{PROJECT_ROOT}/search_run_config.py"
+MAIN_FILE = f"{PROJECT_ROOT}/entries/main.py"
 LOG_FILE = "/tmp/log/main.log"
