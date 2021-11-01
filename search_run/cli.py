6e1e4e4b596b943675cccbee3504cc595b22ef16
JeanCarloMachado <jean.machado@getyourguide.com>
2021-11-01 17:00:09 +0100

    automatic commit

--- search_run/cli.py
+++ search_run/cli.py
@@ -41,7 +41,7 @@ class SearchAndRunCli:
             """returns the shell command to perform to get all get_options_cmd
             and generates the side-effect of createing a new cache file if it does not exist"""
             configuration_file_name = (
-                self.configuration.generate_and_get_cached_file_name()
+                self.configuration_exporter.generate_and_get_cached_file_name()
             )
             cmd = f' cat "{configuration_file_name}" '
             return cmd
