ae80a6d26c6a708ac41c99149a63e063b9278b92
JeanCarloMachado <jean.machado@getyourguide.com>
2022-01-15 18:56:18 +0100

    read and write from redis working

---
+++ tests/test_redis.py
@@ -0,0 +1,26 @@
+import redis
+
+host = "localhost"
+port = 6378
+key_name = "last_consumed_keys"
+
+
+def get_redis_client():
+    return redis.StrictRedis(host=host, port=port)
+
+
+def test_write_to_redis():
+    redis_client = get_redis_client()
+    redis_client.lpush(key_name, "enable_sentry_remote_mode")
+
+
+def test_read_from_redis():
+    redis_client = get_redis_client()
+    result = redis_client.lrange(key_name, 0, 10)
+    return result
+
+
+if __name__ == "__main__":
+    import fire
+
+    fire.Fire()
