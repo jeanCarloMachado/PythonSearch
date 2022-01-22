8af03f4c339fe821a5a11c62511ca41634cee5b6
JeanCarloMachado <jean.machado@getyourguide.com>
2022-01-22 10:42:54 +0100

    bump version up

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="python-search",
-    version="0.03",
+    version="0.07",
     author="Jean Carlo Machado",
     author_email="machado.c.jean@gmail.com",
     description="Search over python dictionaries",
