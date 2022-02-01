206531b6a8456a4792293c4d24dcc51f7e57bc64
JeanCarloMachado <jean.machado@getyourguide.com>
2022-02-01 21:24:03 +0100

    requirements installed

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ setuptools.setup(
     author_email="machado.c.jean@gmail.com",
     description="Search over python dictionaries",
     long_description="",
+    install_requires=[
+        "redis",
+        "fire",
+    ],
     long_description_content_type="text/markdown",
     url="https://github.com/jeanCarloMachado/search_run",
     packages=setuptools.find_packages(),
