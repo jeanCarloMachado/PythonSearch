9973b6d83cc0541d5f63593d9d2177348fb632fd
JeanCarloMachado <jean.machado@getyourguide.com>
2022-03-22 21:59:17 +0100

    automatic commit

---
+++ search_run/search_ui/simple_gui.py
@@ -0,0 +1,13 @@
+# !/usr/bin/env python
+import fire
+
+import PySimpleGUI as sg
+
+class Cli():
+    def run(self):
+        sg.Window(title="Hello World", layout=[[]], margins=(100, 50)).read()
+        return "hello world"
+
+if __name__ == '__main__':
+    fire.Fire(Cli)
+
