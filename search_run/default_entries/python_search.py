1f4f074b5f0b674ace5fea909fcdae5fc698c8bd
JeanCarloMachado <jean.machado@getyourguide.com>
2022-04-16 12:00:53 +0200

    move things to default entries

---
+++ search_run/default_entries/python_search.py
@@ -0,0 +1,30 @@
+from search_run.base_configuration import EntriesGroup
+
+
+class PythonSearch(EntriesGroup):
+    """
+    PythonSearch entries
+    """
+
+    commands = {
+        "search run search focus or open": {
+            "focus_match": "PythonSearchWindow",
+            "cmd": "nice -19 search_run search",
+        },
+        "start search run search capslock": {
+            "focus_match": "launcher",
+            "cmd": "nice -19 search_run search",
+            "i3_shortcut": "Mod3+space",
+        },
+        "register snippet search run": {
+            "cmd": "search_run register_snippet_clipboard",
+        },
+        "recompute rank search run cleaning the cache regenerate": {
+            "new-window-non-cli": True,
+            "cmd": "search_run ranking generate --recompute_ranking=True",
+        },
+        "compute rerank search run": {
+            "new-window-non-cli": True,
+            "cmd": "search_run ranking recompute_rank",
+        },
+    }
