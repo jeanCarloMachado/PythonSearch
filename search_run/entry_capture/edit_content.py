5bc1cc60cdf1a3a64ee899cbbfd6d8dbd0702740
Jean Machado <jean.machado@Jeans-MacBook-Air.local>
2022-06-05 20:04:43 +0200

    make edit key work

--- search_run/entry_capture/edit_content.py
+++ search_run/entry_capture/edit_content.py
@@ -58,7 +58,7 @@ class EditKey:
     def _edit_config(self, file_name: str, line: Optional[int] = 30, dry_run=False):
         """ "edit a configuration file given the name and line"""
         cmd: str = (
-            f"MY_TITLE='GrimorieSearchRun' runFunction terminal_run 'cd"
+            f"MY_TITLE='GrimorieSearchRun' kitty bash -c 'cd"
             f" {self.configuration.get_project_root()} "
             f"; {config.EDITOR} {file_name} +{line}' "
         )
