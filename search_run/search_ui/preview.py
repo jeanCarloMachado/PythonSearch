140c5bd7fc56b4df1f3289dea76435408ae03267
JeanCarloMachado <jean.machado@getyourguide.com>
2022-06-13 20:50:11 +0200

    Automatic commit snapshot

--- search_run/search_ui/preview.py
+++ search_run/search_ui/preview.py
@@ -2,12 +2,16 @@
 
 class Preview:
     def display(self, entry_text):
-        key = entry_text.split(':')[-1]
+        """
+        Prints the
+        """
+        key = entry_text.split(':')[0]
         json_str = entry_text.replace(key + ':', '')
         import json
 
         try:
             data = json.loads(json_str)
             print(json.dumps(data, indent=1))
-        except:
+        except BaseException as e:
+            print('Error decoding:')
             print(json_str)
