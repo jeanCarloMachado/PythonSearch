38394262e2b2fb5d685a88a00f8f3a68473b2f34
Jean Carlo Machado <jean.machado@getyourguide.com>
2022-06-14 18:26:25 +0200

    add support to mac shortcuts

--- search_run/shortcut/mac.py
+++ search_run/shortcut/mac.py
@@ -8,11 +8,23 @@ class Mac:
         print("Generating macos shortctus")
 
         shortcut_found = False
+        shortcut_number = 1
         for key, content in list(self.configuration.commands.items()):
             if type(content) is dict and "mac_shortcut" in content:
                 print(f"Generating shortcut for {key}")
+                print(self._entry(content['mac_shortcut'], key, shortcut_number))
+                shortcut_number+=1
                 shortcut_found = True
 
         if not shortcut_found:
             print("No shortcut found for mac" )
 
+    def _entry(self, shortcut, key, number):
+        return f"""
+[shortcut{number}]
+shortcut = {shortcut}
+action = {key}
+command = search_run run_key '{key}'
+workdir = 
+enabled = yes
+        """
