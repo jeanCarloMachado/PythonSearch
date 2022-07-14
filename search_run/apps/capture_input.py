4a64e94471eebf3416fee9490e3fb75b0a833a24
Jean Carlo Machado <jean.machado@getyourguide.com>
2022-07-14 08:28:54 +0200

    improve collect data ui

--- search_run/apps/capture_input.py
+++ search_run/apps/capture_input.py
@@ -3,25 +3,30 @@ import sys
 import fire
 
 
-def launch(name="Enter Data:"):
+def launch(name="Enter Data"):
     """
     Launch the data capture GUI.
     """
     import PySimpleGUI as sg
 
+    font_size = 12
     sg.theme("SystemDefault1")
 
     layout = [
-        [sg.Text(name)],
-        [sg.Input(key="content")],
+        [
+            sg.Input(
+                key="content",
+            )
+        ],
         [sg.Button("Continue", key="write")],
     ]
 
-    window = sg.Window("Capture input", layout, finalize=True)
+    window = sg.Window(name, layout, finalize=True, font=("Helvetica", font_size))
+    window["content"].bind("<Return>", "_Enter")
 
     while True:
         event, values = window.read()
-        if event == "write":
+        if event == "write" or event == "content_Enter":
             break
         if event == sg.WINDOW_CLOSED:
             sys.exit(1)
