7ff5da3852496f181fb7f69da2dbd150f078b37f
Jean Carlo Machado <jean.machado@getyourguide.com>
2022-09-02 09:46:22 +0200

    german

--- python_search/search_ui/preview.py
+++ python_search/search_ui/preview.py
@@ -29,28 +29,29 @@ class Preview:
 
     def _print_values(self, data):
         print("")
-        print(self._color_str(data['value'], self._get_color_for_type(data['type'])))
+        print(self._color_str(data["value"], self._get_color_for_type(data["type"])))
         print("")
         print(f"Key: {self._color_str(data['key'], Fore.YELLOW)}")
-        if 'position' in data:
-            print("Position: " + data['position'])
+        print("Type: " + data['type'])
+        if "position" in data:
+            print("Position: " + data["position"])
 
-        if 'created_at' in data:
-            print("Created at: " + data['created_at'])
-            print("Entry Age: " + data['entry_age'])
+        if "created_at" in data:
+            print("Created at: " + data["created_at"])
+            print("Entry Age: " + data["entry_age"])
 
-        if 'tags' in data:
-            print("Tags: " + data['tags'])
+        if "tags" in data:
+            print("Tags: " + data["tags"])
+
 
     def _get_color_for_type(self, type):
-        if type == 'cmd':
+        if type == "Cmd":
             return Fore.RED
 
-        if type in ['url', 'file']:
+        if type in ["Url", "File"]:
             return Fore.GREEN
 
         return Fore.BLUE
-
 
     def _color_str(self, a_string, a_color) -> str:
         return f"{a_color}{a_string}{Fore.RESET}"
@@ -59,46 +60,48 @@ class Preview:
         print_values = entry_text.split(":")
         key = print_values[0]
         # the entry content is after the key + a ":" character
-        serialized_content = entry_text[len(key) + 1:]
+        serialized_content = entry_text[len(key) + 1 :]
 
         print_values = {}
-        print_values['type'] = "Unknown"
-        print_values['key'] = key
+        print_values["type"] = "Unknown"
+        print_values["key"] = key
         entry_data = self._load_key_data(key)
         if "url" in entry_data:
-            print_values['value'] = entry_data.get("url")
-            print_values['type'] = 'Url'
+            print_values["value"] = entry_data.get("url")
+            print_values["type"] = "Url"
 
         if "file" in entry_data:
-            print_values['value'] = entry_data.get("file")
-            print_values['type'] = 'File'
+            print_values["value"] = entry_data.get("file")
+            print_values["type"] = "File"
 
         if "snippet" in entry_data:
-            print_values['value'] = entry_data.get("snippet")
-            print_values['type'] = 'Snippet'
+            print_values["value"] = entry_data.get("snippet")
+            print_values["type"] = "Snippet"
 
         if "cli_cmd" in entry_data or "cmd" in entry_data:
-            print_values['value'] = entry_data.get("cli_cmd", entry_data.get("cmd"))
-            print_values['type'] = "Cmd"
+            print_values["value"] = entry_data.get("cli_cmd", entry_data.get("cmd"))
+            print_values["type"] = "Cmd"
 
         if "callable" in entry_data:
             value = entry_data.get("callable")
             import dill
-            print_values['value'] = dill.source.getsource(value)
+
+            print_values["value"] = dill.source.getsource(value)
 
         if "created_at" in entry_data:
             from dateutil import parser
+
             creation_date = parser.parse(entry_data["created_at"])
             today = datetime.now()
-            print_values['created_at'] = str(creation_date)
+            print_values["created_at"] = str(creation_date)
             print_values["entry_age"] = str(today - creation_date)
 
-        if 'tags' in entry_data:
-            print_values['tags'] = " ".join(entry_data['tags'])
+        if "tags" in entry_data:
+            print_values["tags"] = " ".join(entry_data["tags"])
 
         decoded_content = self._decode_serialized_data(serialized_content)
-        if 'position' in decoded_content:
-            print_values['position'] = str(decoded_content['position'])
+        if "position" in decoded_content:
+            print_values["position"] = str(decoded_content["position"])
 
         return print_values
 
@@ -113,8 +116,13 @@ class Preview:
         entries = self.configuration.load_entries()
         if not key in entries:
             print()
-            print((f'Key "{self._color_str(key, Fore.RED)}" not found in python search data'))
+            print(
+                (
+                    f'Key "{self._color_str(key, Fore.RED)}" not found in python search data'
+                )
+            )
             import sys
+
             sys.exit(0)
 
         return entries[key]
