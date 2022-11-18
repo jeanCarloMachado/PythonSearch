dca043219ca957f7d03e866a9acd5b509174a74e
Jean Carlo Machado <jean.machado@getyourguide.com>
2022-11-18 08:14:24 +0000

    detected arm architecture during conda download

--- container/setup.sh
+++ container/setup.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 
 # poetry setup
 #curl -sSL https://install.python-poetry.org | python3 -
@@ -7,9 +8,11 @@
 # conda installation
 export PATH="/root/miniconda3/bin/:$PATH"
 FILE_NAME=Miniconda3-py39_4.12.0-Linux-x86_64.sh
-if [[ $( dpkg --print-architecture) == "arm64"]]; then
+if [[ $( dpkg --print-architecture) -eq "arm64" ]]; then
+  echo "Arm architecture detected"
   FILE_NAME=Miniconda3-py39_4.12.0-Linux-aarch64.sh
 fi
+
 wget https://repo.anaconda.com/miniconda/$FILE_NAME
 chmod +x $FILE_NAME
 ./$FILE_NAME -b -f
