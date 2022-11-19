a2eb8818de3cf11dc03751a099bbef1eda42070f
Jean Carlo Machado <jean.machado@getyourguide.com>
2022-11-19 12:37:15 +0000

    Automatic changes

--- sync_hosts.sh
+++ sync_hosts.sh
@@ -6,6 +6,7 @@ function current_branch() {
   git branch 2> /dev/null | grep "*" | cut -d" " -f2
 }
 
+echo 'Syncing entries project'
 cd /entries
 
 git add .
@@ -13,6 +14,7 @@ git commit -m 'Automatic changes'
 git pull origin `current_branch`
 git push origin `current_branch`
 
+echo 'Syncing python search project'
 cd /src
 git add .
 git commit -m 'Automatic changes'
