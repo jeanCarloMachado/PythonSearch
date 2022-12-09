2903ba5690736dedf46b8144cbccd09b89c5c645
Jean Carlo Machado <jean.machado@getyourguide.com>
2022-12-09 08:43:16 +0100

    add data exporter

--- python_search/data_exporter.py
+++ python_search/data_exporter.py
@@ -3,13 +3,23 @@ from python_search.data_ui.homepage import extract_value_from_entry
 
 
 class DataExporter:
+    blacklisted_terms = [
+        "email" ,"insurance", "token", "private", 'endereco', "password", "secret", "passport", "passaporte", 'telefone', 'phone', 'celular', 'cellphone', 'cpf',
+    ]
     def export_as_text(self):
-        entries = ConfigurationLoader().load_entries()
 
+        entries = ConfigurationLoader().load_entries()
 
         data = ''
         for key, value in entries.items():
-            serializeable_value = extract_value_from_entry(value)
+            if any([blackelisted_entry in key.lower() for blackelisted_entry in self.blacklisted_terms]):
+                print("Skipping blacklisted key: ", key)
+                continue
+
+            if 'private' in value:
+                continue
+
+            serializeable_value = extract_value_from_entry(value).replace('\n', ' ')
             data = data + f"{key}={serializeable_value}\n"
 
         with open("exported_entries.txt", "w") as f:
