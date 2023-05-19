f95f364455e1d13201fcb75bd28fdbef919d5f06
Jean Carlo Machado <jean.machado@getyourguide.com>
2023-05-19 12:56:04 +0200

    style

--- python_search/llm_next_item_predictor/t5/config.py
+++ python_search/llm_next_item_predictor/t5/config.py
@@ -1,7 +1,11 @@
 
 import os
 HOME = os.environ['HOME']
+
 class T5ModelConfig:
+    """
+    Configuration for the T5 model
+    """
     VERSION = 'v2'
     NEW_MODEL_TARGET_DIRECTORY = 't5_llm_models/' + VERSION
     BASE_MODEL_PATH = HOME + '/.python_search/t5_llm_models'
