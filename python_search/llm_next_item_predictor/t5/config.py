ec063bb7c1f48ec298f68479e81d86f91a1ec2c5
Jean Carlo Machado <jean.machado@getyourguide.com>
2023-05-21 18:26:27 +0200

    cleanups

--- python_search/llm_next_item_predictor/t5/config.py
+++ python_search/llm_next_item_predictor/t5/config.py
@@ -12,7 +12,6 @@ class T5ModelConfig:
     NEW_MODEL_RELATIVE_TARGET_DIRECTORY = "t5_llm_models/model_" + TARGET_VERSION
     BASE_MODEL_PATH = HOME + "/.python_search/t5_llm_models"
     FULL_MODEL_PATH = HOME + "/.python_search/" + NEW_MODEL_RELATIVE_TARGET_DIRECTORY
-    MODEL_T5_V2_EPOCH_16 = BASE_MODEL_PATH + "/v2_epoch_16"
     PRODUCTIONALIZED_MODEL = BASE_MODEL_PATH + "/model_v4_epoch_4"
 
 
