7cf70021114ebbccf11ecdea39e2b6aebf55d1f7
Jean Carlo Machado <jean.machado@getyourguide.com>
2023-06-06 01:23:37 +0200

    rollback to small model

--- python_search/ps_llm/llm_config.py
+++ python_search/ps_llm/llm_config.py
@@ -15,7 +15,7 @@ class LLMConfig:
     NEW_MODEL_RELATIVE_TARGET_DIRECTORY = "t5_llm_models/model_" + TARGET_VERSION
     BASE_MODEL_PATH = BASE_PROJECT_FOLDER + "/t5_llm_models"
     FULL_MODEL_PATH = BASE_PROJECT_FOLDER+ "/" + NEW_MODEL_RELATIVE_TARGET_DIRECTORY
-    NEXT_ITEM_PRODUCTIONALIZED_MODEL = '/Users/jean.machado/Library/CloudStorage/GoogleDrive-jean.machado@getyourguide.com/My Drive/PythonSearchDataInDrive/t5_llm_models/model_v14_epoch_9'
+    NEXT_ITEM_PRODUCTIONALIZED_MODEL = BASE_MODEL_PATH + '/model_v12_epoch_3'
     SUMMARIZATION_PRODUCTIONALIZED_MODEL = NEXT_ITEM_PRODUCTIONALIZED_MODEL
     BASE_MODEL_TO_TRAIN_OVER = 't5-base'
     BASE_DATASET_FOLDER = BASE_PROJECT_FOLDER + "/datasets"
