5a0864b06c62c6b9e022347ebfbab39b0e05c5cc
Jean Carlo Machado <jean.machado@getyourguide.com>
2023-08-18 08:47:53 +0200

    restore windows focus for register new

--- python_search/host_system/windows_focus.py
+++ python_search/host_system/windows_focus.py
@@ -4,8 +4,17 @@ from concurrent.futures import ThreadPoolExecutor
 
 class Focus:
 
+
     def focus_register_new(self):
-        return self.focus_window("python3", "Register New")
+        result = os.system("""
+        osascript -e 'tell application "System Events" to tell process "python3"
+                set frontmost to true
+                windows where title contains "Register New"
+                if result is not {} then perform action "AXRaise" of item 1 of result
+end tell' 
+        """)
+
+        return True if result == 0 else False
 
 
     def focus_window(self, app, title):
