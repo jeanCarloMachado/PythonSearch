31461f5ec1c15c004f4124a5d0de1c19bbf4f9ce
Jean Carlo Machado <jean.machado@getyourguide.com>
2023-08-19 17:04:58 +0200

    remove old files

---
+++ website/content/first.md
@@ -0,0 +1,4 @@
+Title: My First Review
+Date: 2010-12-03 10:20
+
+Following is a review of my favorite mechanical keyboard.
---
+++ website/pelicanconf.py
@@ -0,0 +1,31 @@
+AUTHOR = 'JeanCarloMachado'
+SITENAME = 'PythonSearch'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Berlin'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ( ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+THEME = 'notmyidea'
+
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
