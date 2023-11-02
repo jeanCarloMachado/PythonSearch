ecf33db440ef9c947e84a34ae151d25fb9d9971f
Jean Carlo Machado <jean.machado@getyourguide.com>
2023-11-02 20:48:15 +0100

    commented profiel

--- python_search/__init__.py
+++ python_search/__init__.py
@@ -11,13 +11,12 @@ os.environ['DD_LOGS_INJECTION'] = 'true'
 os.environ['DD_PROFILING_ENABLED'] = 'true'
 #os.environ['DD_TRACE_DEBUG'] = 'true'
 
-from ddtrace.profiling import Profiler
-
-import sys
-prof = Profiler(
-    env="local",  # if not specified, falls back to environment variable DD_ENV
-    service="python_search",  # if not specified, falls back to environment variable DD_SERVICE
-    version=str(sys.argv[0]),   # if not specified, falls back to environment variable DD_VERSION
-    tags={'entrypoint': sys.argv[0]},
-)
-prof.start()
+#from ddtrace.profiling import Profiler
+#import sys
+#prof = Profiler(
+#    env="local",  # if not specified, falls back to environment variable DD_ENV
+#    service="python_search",  # if not specified, falls back to environment variable DD_SERVICE
+#    version=str(sys.argv[0]),   # if not specified, falls back to environment variable DD_VERSION
+#    tags={'entrypoint': sys.argv[0]},
+#)
+#prof.start()
