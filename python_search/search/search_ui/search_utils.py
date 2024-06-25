5a50cc1ceb3432643b414d1f7d4c66a6f3174732
Jean Carlo Machado <jean.machado@getyourguide.com>
2024-06-25 22:58:28 +0200

    datadog integration worked

--- python_search/search/search_ui/search_utils.py
+++ python_search/search/search_ui/search_utils.py
@@ -1,9 +1,11 @@
 
-from datadog import initialize, statsd
+def setup_datadog():
+    from datadog import initialize, statsd
+    options = {
+        'statsd_host':'127.0.0.1',
+        'statsd_port':8125,
+    }
 
-options = {
-    'statsd_host':'127.0.0.1',
-    'statsd_port':8125
-}
+    initialize(**options)
 
-initialize(**options)
+    return statsd
---
+++ tests/test_datadog.py
@@ -0,0 +1,11 @@
+
+
+def test_datadog():
+    from python_search.search.search_ui.search_utils import setup_datadog
+
+    statsd = setup_datadog()
+    assert statsd is not None
+    # change the range to debug when needed
+    for i in range(1):
+        print(i)
+        statsd.increment('test.increment')
